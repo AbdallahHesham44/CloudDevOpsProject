f23b2f4220f5e2a762008a1164302bcf7e6ab814
Abdallah Hesham <124697660+AbdallahHesham44@users.noreply.github.com>
2024-12-26 00:26:35 +0200

    Delete terraform directory

--- terraform/modules/ec2/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "ami_id" {}
-variable "instance_type" {}
-variable "subnet_id" {}
-variable "security_group_id" {}
-variable "tags" {
-  type = map(string)
-}
-variable "cloudwatch_config_ssm_key" {}
