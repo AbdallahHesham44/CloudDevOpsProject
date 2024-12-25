f3f26f54cae8e3bf294ca0c835e677b37dcfa9d3
Abdallah Hesham <124697660+AbdallahHesham44@users.noreply.github.com>
2024-12-26 00:27:54 +0200

    Add files via upload

---
+++ terraform/modules/ec2/outputs.tf
@@ -0,0 +1,15 @@
+output "ec2_public_ip" {
+  
+   //value       = module.ec2_instanc
+   
+   value = aws_instance.ec2.public_ip
+
+  description = "The public IP address of the EC2 instance"
+}
+
+output "instance_id" {
+  value = aws_instance.ec2.id
+}
+output "public_ip" {
+  value = aws_instance.ec2.public_ip
+}
---
+++ terraform/outputs.tf
@@ -0,0 +1,9 @@
+
+
+output "instance_id" {
+  value = module.ec2_instance.instance_id
+}
+
+output "ec2_public_ips" {
+  value = module.ec2_instance.public_ip
+}
