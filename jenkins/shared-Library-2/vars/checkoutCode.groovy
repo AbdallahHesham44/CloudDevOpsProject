72f45e1151d552d4b924cced50422033a98e31e3
Abdallah Hesham <124697660+AbdallahHesham44@users.noreply.github.com>
2024-12-30 01:00:59 +0200

    Update checkoutCode.groovy

--- jenkins/shared-Library-2/vars/checkoutCode.groovy
+++ jenkins/shared-Library-2/vars/checkoutCode.groovy
@@ -1,6 +1,7 @@
-// vars/gitCheckout.groovy
-def call(String repoUrl, String branch) {
-    
-    git branch: branch, url: repoUrl
-    
+def call() {
+    stage('Checkout') {
+        steps {
+            git branch: 'main', url: 'https://github.com/AbdallahHesham44/CloudDevOpsProject.git'
+        }
+    }
 }
