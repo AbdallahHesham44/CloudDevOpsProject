4e713e9f1ef77960f3c322909e851a98ed10a0b7
Abdallah Hesham <124697660+AbdallahHesham44@users.noreply.github.com>
2024-12-30 00:28:45 +0200

    Create dockerLogin.groovy

---
+++ jenkins/shared-Library-2/vars/dockerLogin.groovy
@@ -0,0 +1,13 @@
+def call() {
+    stage('Docker Login') {
+        steps {
+            dir('App') {
+                script {
+                    withCredentials([usernamePassword(credentialsId: 'docker-cred', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
+                        sh 'docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD'
+                    }
+                }
+            }
+        }
+    }
+}
